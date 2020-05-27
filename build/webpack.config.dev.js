103b8a2afebfdc48e41f959cb929141a781922a9
全村的希望 <1933310737@qq.com>
2020-05-27 14:32:14 +0800

    gx

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,12 +1,24 @@
 const path = require("path");
 const { smart } = require("webpack-merge"); //引入插件
 const mainConfig = require("./webpack.config"); //将公共配置文件导入
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const config = require("./config");
 module.exports = smart(mainConfig, {
   mode: "development",
   devtool: "source-map",
   devServer: {
-    port: 8899,
-    open: true,
+    port: config.port,
+    open: false,
     historyApiFallback: true,
+    overlay: true,
+    quiet: true,
   },
+  plugins: [
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: [`Success🚀服务器运行在http://localhost:${config.port}`],
+        clearConsole: true,
+      },
+    }),
+  ],
 });
